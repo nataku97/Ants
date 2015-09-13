af6acb10452ac6467cf0ce6eccd48d898d1b519f
Nate <gnate@rams.colostate.edu>
2015-09-12 19:25:26 -0500

    removed old files

--- antsController.js
+++
@@ -1,9 +0,0 @@
-/*
-	This initialized the app data for ant simulation.
-*/
-angular.module('Ants', []).controller('antsCtrl',  function($scope) {
-	$scope.teams = [ new Colony('Green', '#3CB371'),
-					 new Colony('Blue', '#4169E1'),
-					 new Colony('Red', '#4169E1'), 
-					 new Colony('Gold', '#FFD700')];
-});
